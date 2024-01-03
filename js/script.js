1cc12b9b40aca84f40a1c20b0ed33a8bce5d6d58
Pedrosavioo <pedrosaviolima618@gmail.com>
2024-01-03 19:45:50 -0300

    update

--- js/script.js
+++ js/script.js
@@ -2,19 +2,19 @@ const btnBlack = document.querySelector("#btn-black")
 const btnWhite = document.querySelector("#btn-white")
 const btnBlue = document.querySelector("#btn-blue")
 
-const img = document.querySelector("#img")
+const img = document.querySelector("#img-model")
 
 btnBlack.addEventListener("click", function(){
     img.src = 'assets/Black.png';
-    img.alt = 'Imagem 2';
+    img.alt = 'Headphone Tune 520BT black';
 })
 
 btnWhite.addEventListener("click", function(){
     img.src = 'assets/White.png';
-    img.alt = 'Imagem 2';
+    img.alt = 'Headphone Tune 520BT white';
 })
 
-btnBlue.addEventListener("click", ()=>{
+btnBlue.addEventListener("click", function(){
     img.src = 'assets/Blue.png';
-    img.alt = 'Imagem 2';
+    img.alt = 'Headphone Tune 520BT blue';
 })
